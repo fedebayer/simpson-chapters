af5ec404e13c907d2b7ba6302317ce2dfd34e40e
FedeBayer <89879671+FedeBayer@users.noreply.github.com>
2021-11-24 17:40:26 -0300

    Mejorado el buscador avanzado

--- Model/ChapterModel.php
+++ Model/ChapterModel.php
@@ -67,36 +67,8 @@ class ChapterModel
 
     function getListBySearch($nombre = null, $temporada = null, $estreno = null, $director = null, $guionista = null)
     {
-        $sql = "SELECT c.*, GROUP_CONCAT(g.nombre) AS guionistas, GROUP_CONCAT(g.id_guionista) AS id_guionistas, d.nombre_director AS director FROM ( ( guionista_de_x_capitulo AS r RIGHT JOIN capitulo AS c ON c.id_capitulo = r.id_capitulo ), director d) LEFT JOIN guionista AS g ON g.id_guionista = r.id_guionista AND d.id_director = c.id_director";
-        if ($nombre != null)
-            $sql .= " WHERE LOWER(c.nombre) LIKE LOWER ('%$nombre%')";
-        if ($temporada != null) {
-            if ($nombre == null)
-                $sql .= " WHERE LOWER(c.temporada) LIKE LOWER('%$temporada%')";
-            else
-                $sql .= " AND LOWER(c.temporada) LIKE LOWER('%$temporada%')";
-        }
-        if ($estreno != null) {
-            if ($nombre == null && $temporada == null)
-                $sql .= " WHERE LOWER(c.estreno) LIKE LOWER('%$estreno%')";
-            else
-                $sql .= " AND LOWER(c.estreno) LIKE LOWER('%$estreno%')";
-        }
-        if ($director != null) {
-            if ($nombre == null && $temporada == null && $estreno == null)
-                $sql .= " WHERE LOWER(d.nombre_director) LIKE LOWER('%$director%')";
-            else
-                $sql .= " AND LOWER(d.nombre_director) LIKE LOWER('%$director%')";
-        }
-        if ($guionista != null) {
-            if ($nombre == null && $temporada == null && $estreno == null && $director == null)
-                $sql .= " WHERE LOWER(g.nombre) LIKE LOWER('%$guionista%')";
-            else
-                $sql .= " AND LOWER(g.nombre) LIKE LOWER('%$guionista%')";
-        }
-        $sql .= " GROUP BY c.id_capitulo";
-        $query = $this->db->prepare($sql);
-        $query->execute();
+        $query = $this->db->prepare("SELECT c.*, GROUP_CONCAT(g.nombre) AS guionistas, GROUP_CONCAT(g.id_guionista) AS id_guionistas, d.nombre_director AS director FROM ( ( guionista_de_x_capitulo AS r RIGHT JOIN capitulo AS c ON c.id_capitulo = r.id_capitulo ), director d) LEFT JOIN guionista AS g ON g.id_guionista = r.id_guionista AND d.id_director = c.id_director WHERE LOWER(c.nombre) LIKE LOWER(?) AND LOWER (c.temporada) LIKE LOWER(?) AND LOWER(c.estreno) LIKE LOWER(?) AND LOWER(d.nombre_director) LIKE LOWER(?) AND LOWER(g.nombre) LIKE LOWER(?) GROUP BY c.id_capitulo");
+        $query->execute(["%$nombre%", "%$temporada%", "%$estreno%", "%$director%", "%$guionista%"]);
         $chapters = $query->fetchAll(PDO::FETCH_OBJ);
         return $chapters;
     }
